cd5b6c23eca34413b923403c92ae353395aee511
Cao Viết Huy <hieppro138@gmail.com>
2023-10-29 20:32:46 +0700

    refactor: set up layout

---
+++ frontend/components/section-print.tsx
@@ -0,0 +1,11 @@
+type SectionPrintProps = {}
+
+const SectionPrint = ({}: SectionPrintProps) => {
+  return (
+    <div className="flex items-center justify-center w-full h-[800px] bg-yellow-700">
+      <h1 className="text-4xl">Section Print</h1>
+    </div>
+  )
+}
+
+export default SectionPrint
