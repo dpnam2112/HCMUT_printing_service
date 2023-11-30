7327aa0de100a0395248db83e55ce327fe7753ec
Cao Viết Huy <hieppro138@gmail.com>
2023-11-30 17:22:27 +0700

    refactor: add logic trasaction history

--- frontend/components/admin/transaction-history-view.tsx
+++
@@ -1,9 +0,0 @@
-const TransactionHistoryView = () => {
-  return (
-    <div className="w-full h-full flex items-center justify-center text-lg bg-blue-800">
-      Transaction History View
-    </div>
-  );
-};
-
-export default TransactionHistoryView;
