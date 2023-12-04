7930a55dc80d10841585f1196ac0733225e3318a
Cao Viết Huy <hieppro138@gmail.com>
2023-12-04 18:12:44 +0700

    refactor: logic add printer

--- frontend/components/admin/printer-adding/printer-adding-view.tsx
+++ frontend/components/admin/printer-adding/printer-adding-view.tsx
@@ -1,31 +1,66 @@
 import { Button, Table, TextField, Tooltip, TextArea } from "@radix-ui/themes";
-import MenuFacility from "../../menus/menu-facility";
 import { useState } from "react";
-import {
-  MENU_BUILDING_CS1,
-  MENU_BUILDING_CS2,
-  MENU_FACILITY,
-  MENU_ROOM,
-} from "../printing-history-view/models/constant";
-import MenuBuildingCS1 from "../../menus/menu-building-cs1";
-import MenuBuildingCS2 from "../../menus/menu-building-cs2";
-import MenuRoom from "../../menus/menu-room";
+import InputNumberField from "../../input-number-field";
+import InputTextField from "../../input-text-field";
+import { Printer } from "../../../models/types";
+import networkService from "../../../models/network-service";
+import toast from "react-hot-toast";
 
 const PrinterAddingView = () => {
-  const [selectedFacility, setSelectedFacility] = useState<MENU_FACILITY>(
-    MENU_FACILITY.LY_THUONG_KIET
-  );
-  const [selectedBuildingCS1, setSelectedBuildingCS1] =
-    useState<MENU_BUILDING_CS1>(MENU_BUILDING_CS1.A1);
-  const [selectedBuildingCS2, setSelectedBuildingCS2] =
-    useState<MENU_BUILDING_CS2>(MENU_BUILDING_CS2.H2);
-  const [selectedRoom, setSelectedRoom] = useState<MENU_ROOM>(MENU_ROOM.R_101);
-  const [printerName, setPrinterName] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [manufacturer, setManufacturer] = useState<string>("");
   const [description, setDescription] = useState<string>("");
+  const [campus, setCampus] = useState<string>("");
+  const [buildingName, setBuildingName] = useState<string>("");
+  const [floor, setFloor] = useState<number>(1);
+  const [roomCode, setRoomCode] = useState<string>("101");
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
 
-  const handleAddPrinter = () => {
-    setPrinterName("");
-    setDescription("");
+  const handleAddPrinter = async () => {
+    if (
+      !name ||
+      !manufacturer ||
+      !description ||
+      !campus ||
+      !buildingName ||
+      !floor ||
+      !roomCode
+    ) {
+      setErrorMessage("Vui lòng điền đầy đủ thông tin!");
+      return;
+    }
+
+    setErrorMessage(undefined);
+
+    const newPrinter: Omit<Printer, "id"> = {
+      name: name,
+      manufacturer: manufacturer,
+      description: description,
+      location: {
+        campus: campus,
+        building_name: buildingName,
+        floor: floor,
+        room_code: roomCode,
+      },
+    };
+
+    const isSuccess = await networkService.addPrinter(newPrinter);
+    if (isSuccess) {
+      setName("");
+      setManufacturer("");
+      setDescription("");
+      setCampus("");
+      setBuildingName("");
+      setFloor(1);
+      setRoomCode("101");
+      setErrorMessage(undefined);
+      toast.success("Thêm máy in thành công!");
+      return;
+    }
+
+    setErrorMessage("Vị trí đặt máy in không tồn tại!");
   };
 
   return (
@@ -40,62 +75,49 @@ const PrinterAddingView = () => {
       </div>
 
       <div className="flex flex-col w-full h-full gap-5">
-        <div className="flex flex-col gap-1">
-          <span className="text-sm font-semibold">Tên máy in</span>
-          <TextField.Input
-            placeholder="Tên máy in"
-            value={printerName}
-            onChange={(e) => {
-              setPrinterName(e.target.value);
-            }}
+        <div className="flex flex-col gap-4">
+          <InputTextField title="Tên máy in" value={name} setValue={setName} />
+          <InputTextField
+            title="Hãng sản xuất"
+            value={manufacturer}
+            setValue={setManufacturer}
           />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <span className="text-sm font-semibold">Đặt tại cơ sở</span>
-          <MenuFacility
-            width="w-[900px]"
-            selectedFacility={selectedFacility}
-            setSelectedFacility={setSelectedFacility}
+          <InputTextField
+            title="Đặt tại cơ sở"
+            value={campus}
+            setValue={setCampus}
           />
-        </div>
+          <InputTextField
+            title="Đặt tại toà"
+            value={buildingName}
+            setValue={setBuildingName}
+          />
+          <InputNumberField
+            title="Đặt tại lầu"
+            value={floor}
+            setValue={setFloor}
+          />
+          <InputTextField
+            title="Đặt tại phòng"
+            value={roomCode}
+            setValue={setRoomCode}
+          />
+          <div className="flex flex-col gap-1">
+            <span className="font-semibold text-sm ml-[2px]">Mô tả máy in</span>
+            <textarea
+              className="rounded h-[100px] p-1"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            ></textarea>
+          </div>
 
-        <div className="flex flex-col gap-1 w-full">
-          <span className="text-sm font-semibold">Đặt tại toà</span>
-          {selectedFacility === MENU_FACILITY.LY_THUONG_KIET ? (
-            <MenuBuildingCS1
-              width="w-[900px]"
-              selectedItem={selectedBuildingCS1}
-              setSelectedItem={setSelectedBuildingCS1}
-            />
-          ) : (
-            <MenuBuildingCS2
-              width="w-[900px]"
-              selectedItem={selectedBuildingCS2}
-              setSelectedItem={setSelectedBuildingCS2}
-            />
+          {errorMessage && (
+            <span className="text-sm font-semibold text-red-500 -mt-2">
+              {errorMessage}
+            </span>
           )}
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <span className="text-sm font-semibold">Đặt tại phòng</span>
-          <MenuRoom
-            width="w-[900px]"
-            selectedItem={selectedRoom}
-            setSelectedItem={setSelectedRoom}
-          />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <span className="text-sm font-semibold">Mô tả máy in</span>
-          <textarea
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-            placeholder="Thêm mô tả cho máy in..."
-            className="border-[#CDCED6] rounded"
-          />
         </div>
 
         <div className="flex items-center justify-end">
