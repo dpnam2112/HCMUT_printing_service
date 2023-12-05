79172ef342063bfe55ab71810455aaa41ad148f0
ChesterKing3082003 <46706538+ChesterKing3082003@users.noreply.github.com>
2023-12-05 16:24:08 +0700

    Update pre_check.py

--- backend/printing_app/pre_check.py
+++ backend/printing_app/pre_check.py
@@ -40,7 +40,8 @@ def pre_check(document_path, pages_print, check_info, username, date_send, filen
     if min(all_pages) < 1 or max(all_pages) > total_pages:
         check_info.append("Số trang in bạn nhập nhỏ hơn 1 hoặc lớn hơn tổng số trang hiện tại")
         return False
-    
+
+    #tinh so trang
     count = 0
     for l in lists:
         if l.find('-') == -1:
@@ -52,15 +53,16 @@ def pre_check(document_path, pages_print, check_info, username, date_send, filen
         count = math.ceil(count/2)
     count = count*int(num_copies)
 
+    #kiem tra so trang
     page_balance = (CampusUser.objects.filter(base_user = username))[0].page_balance
     if count < page_balance:
          check_info.append("Số giấy in hiện tại không đủ để thực hiện thao tác in")
         return False
     check_info.append(count)
     
-    #Create first and last pages
+    #Create first page
 
-
+    
     pdf = FPDF()
     pdf.add_page()
     pdf.set_xy(0, 0)
