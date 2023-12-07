40584cbddd055cf35c2d785466147b92534b267c
ChesterKing3082003 <46706538+ChesterKing3082003@users.noreply.github.com>
2023-12-07 08:18:45 +0700

    Update pre_check.py

--- backend/printing_app/pre_check.py
+++ backend/printing_app/pre_check.py
@@ -8,57 +8,72 @@ from django.conf import settings
 
 def pre_check(document_path, pages_print, check_info, username, date_send, filename, num_copies, side):
 
-    #check format
-    #trim all space
-    pages_print = pages_print.replace(' ','')
 
-    lists = pages_print.split(',')
 
-    for l in lists:
-        if l.find('-') == -1:
-            if len(re.findall("\d+",l)) == 0 or re.findall("\d+",l)[0] != l:
-                check_info.append("Sai định dạng nhập trang in")
-                return False
-        else:
-            if len(re.findall("\d+-\d+",l)) == 0 or re.findall("\d+-\d+",l)[0] != l:
-                check_info.append("Sai định dạng nhập trang in")
-                return False
-
-    #Check pages valid... ex: 3-4, 4-5, 3-6 are allowed but 3-3, 3-2, 6-4
-
-    for l in lists:
-        if l.find('-') != -1:
-            if int(re.findall("\d+",l)[0]) >= int(re.findall("\d+",l)[1]):
-                check_info.append("Số trang trước '-' phải nhỏ hơn số trang ở sau")
-                return False
-    
-    
     file = open(document_path, "rb")
     pdfReader = PyPDF2.PdfReader(file)
 
     total_pages = len(pdfReader.pages)
-    all_pages = re.findall("\d+", pages_print)
-    all_pages = [eval(i) for i in all_pages]
+    count = total_pages
 
-    if (min(all_pages) < 1 or max(all_pages) > total_pages):
-        check_info.append("Số trang in bạn nhập nhỏ hơn 1 hoặc lớn hơn tổng số trang hiện tại")
-        return False
+    if pages_print != 'all':
+            #check format
+        #trim all space
+        pages_print = pages_print.replace(' ','')
 
-    #tinh so trang
-    count = 0
-    for l in lists:
-        if l.find('-') == -1:
-            count = count + 1
-        else:
-            count = count + (int(re.findall("\d+",l)[1]) - int(re.findall("\d+",l)[0])) + 1
+        lists = pages_print.split(',')
 
+        for l in lists:
+            if l.find('-') == -1:
+                if len(re.findall("\d+",l)) == 0 or re.findall("\d+",l)[0] != l:
+                    check_info.append("Sai định dạng nhập trang in")
+                    return False
+            else:
+                if len(re.findall("\d+-\d+",l)) == 0 or re.findall("\d+-\d+",l)[0] != l:
+                    check_info.append("sai định dạng nhập trang in")
+                    return False
+
+        #Check pages valid... ex: 3-4, 4-5, 3-6 are allowed but 3-3, 3-2, 6-4
+
+        for l in lists:
+            if l.find('-') != -1:
+                if int(re.findall("\d+",l)[0]) >= int(re.findall("\d+",l)[1]):
+                    check_info.append("Số trang trước - phải nhỏ hơn số trang ở sau")
+                    return False
+        
+        
+        all_pages = re.findall("\d+", pages_print)
+        all_pages = [eval(i) for i in all_pages]
+
+        if (min(all_pages) < 1 or max(all_pages) > total_pages):
+            check_info.append("Số trang bạn nhập nhỏ hơn 1 hoặc lớn hơn số trang tài liệu bạn cung cấp")
+            return False
+
+        #tinh so trang
+        count = 0
+        for l in lists:
+            if l.find('-') == -1:
+                count = count + 1
+            else:
+                count = count + (int(re.findall("\d+",l)[1]) - int(re.findall("\d+",l)[0])) + 1
+
+        new_pages_print = "1,"
+        for l in lists:
+            if l.find("-") == -1:
+                new_pages_print = new_pages_print + str(int(l) + 1) + ','
+            else:
+                new_pages_print = new_pages_print + str(int(re.findall("\d+",l)[0]) + 1) + "-" + str(int(re.findall("\d+",l)[1]) + 1) + ','
+        new_pages_print = new_pages_print[:-1]
+        check_info.append(new_pages_print)  
+
+        
     if (side != 'one-sided'):
-        count = math.ceil(count/2)
+            count = math.ceil(count/2)
     count = count*int(num_copies)
-
     #kiem tra so trang
-    if not settings.FRONTEND_DEV:
+    if not settings.FRONTEND_DEV and username.is_superuser:
         p = CampusUser.objects.filter(base_user = username)
+        print(p[0].page_balance)
         if count > p[0].page_balance:
             check_info.append("Số giấy của bạn hiện không đủ")
             return False
@@ -91,12 +106,5 @@ def pre_check(document_path, pages_print, check_info, username, date_send, filen
     os.remove(path)
 
     #edit pages_print
-    new_pages_print = "1,"
-    for l in lists:
-        if l.find("-") == -1:
-            new_pages_print = new_pages_print + str(int(l) + 1) + ','
-        else:
-            new_pages_print = new_pages_print + str(int(re.findall("\d+",l)[0]) + 1) + "-" + str(int(re.findall("\d+",l)[1]) + 1) + ','
-    new_pages_print = new_pages_print[:-1]
-    check_info.append(new_pages_print)  
+    
     return True
